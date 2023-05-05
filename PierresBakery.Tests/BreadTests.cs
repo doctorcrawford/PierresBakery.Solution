7c6ddd919b0748204bcd991b2f7bb4401f9992b3
Kyle Crawford <kcraw217@gmail.com>
2023-05-05 09:36:23 -0700

    build project base

---
+++ PierresBakery.Tests/BreadTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+// using System.Linq;
+using System.Collections.Generic;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    
+  }
+}
---
+++ PierresBakery/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PierresBakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      
+    }
+  }
+}
