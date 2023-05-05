3c2a476b0f531dbb2b5e59f7599bd124c75b9592
Kyle Crawford <kcraw217@gmail.com>
2023-05-05 13:17:03 -0700

    start UI with welcome banner

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using PierresBakery.Models;
+using PierresBakery.UserInterfaceModels;
 
 namespace PierresBakery
 {
@@ -6,7 +8,20 @@ namespace PierresBakery
   {
     static void Main()
     {
-      
+      BakeryRun();
+    }
+
+    private static void BakeryRun()
+    {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine($"{Banner.Welcome}");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("We have a mix of various types of bread and pastries for you to purchase.");
+      Console.WriteLine("First of, here are our bread options:");
+      Console.WriteLine("");
+      Console.WriteLine("Would you like to purchase any bread? (y/n)");
+      Console.WriteLine("Type 'y' to proceed to bread selection");
+      Console.WriteLine("Type 'n' to move onto our pastries");
     }
   }
 }
