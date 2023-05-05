464c2a428560357407c110f90e748c1f7c86e4b9
Kyle Crawford <kcraw217@gmail.com>
2023-05-05 10:39:06 -0700

    add GetCost method to calculate cost of loaves, including 2 for 1 deal

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -10,11 +10,19 @@ namespace PierresBakery.Tests
   public class BreadTests
   {
     [TestMethod]
-    public void BreadConstructor_CreatesInstanceOfTriangleWithCount_Int()
+    public void BreadConstructor_CreatesInstanceOfBreadWithCount_Int()
     {
       Bread newBread = new Bread(4);
       int expected = 4;
       Assert.AreEqual(expected, newBread.Count);
+    }
+
+    [TestMethod]
+    public void GetCost_GetsPriceBasedOffAmtOfBread_Int()
+    {
+      Bread newBread = new Bread(7);
+      int expected = 25;
+      Assert.AreEqual(expected, newBread.GetCost());
     }
   }
 }
--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -6,12 +6,17 @@ namespace PierresBakery.Models
   public class Bread
   {
     public int Count { get; set; }
-    public int Cost {get; set; }
+    public int Price {get; set; }
 
     public Bread (int count)
     {
       Count = count;
-      Cost = 5;
+      Price = 5;
+    }
+
+    public int GetCost()
+    {
+      return (Count - (int)(Count / 3)) * Price;
     }
   }
 }
