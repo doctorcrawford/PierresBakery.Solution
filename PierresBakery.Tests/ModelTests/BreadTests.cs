674baa6e95fcafca0160c7e7a0fc8c5a661d8c8e
Kyle Crawford <kcraw217@gmail.com>
2023-05-05 12:02:33 -0700

    add Bread Types list and field

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -12,7 +12,7 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBreadWithCount_Int()
     {
-      Bread newBread = new Bread(4);
+      Bread newBread = new Bread("Challah", 4);
       int expected = 4;
       Assert.AreEqual(expected, newBread.Count);
     }
@@ -20,9 +20,24 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void GetCost_GetsPriceBasedOffAmtOfBread_Int()
     {
-      Bread newBread = new Bread(7);
+      Bread newBread = new Bread("Challah", 7);
       int expected = 25;
       Assert.AreEqual(expected, newBread.GetCost());
+    }
+
+    [TestMethod]
+    public void FindType_LooksForBreadType_String()
+    {
+      string expected = "Challah";
+      Assert.AreEqual(expected, Bread.BreadTypes[1]);
+    }
+
+    [TestMethod]
+    public void GetType_GetsTypeOfBreadType_String()
+    {
+      Bread newBread = new Bread("Challah", 7);
+      string expected = "Challah";
+      Assert.AreEqual(expected, newBread.Type);
     }
   }
 }
--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Collections.Generic;
 
 namespace PierresBakery.Models
 {
   public class Bread : Item
   {
-    public Bread (int count)
+
+    public static List<string> BreadTypes = new List<string> {"Sourdough", "Challah", "Baguette"};
+
+    public Bread (string type, int count)
     {
+      Type = type;
       Count = count;
       Price = 5;
       Deal = 3;
