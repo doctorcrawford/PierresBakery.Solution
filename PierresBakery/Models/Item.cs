3eef1e94389348d85f0d16c0c86484ca4d982e4f
Kyle Crawford <kcraw217@gmail.com>
2023-05-09 18:15:03 -0700

    clean up code

--- PierresBakery/Models/Item.cs
+++ PierresBakery/Models/Item.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-
 namespace PierresBakery.Models
 {
   public abstract class Item
   {
-    public string Type { get; set; }
+    public string Type { get; set; } = "";
     public int Count { get; set; }
     public int Price { get; set; }
     public int Deal { get; set; }
